f905370c280b86ad3fc51d95bcaea3050181ba40
chiaxr <chia.xiangrong@gmail.com>
2018-06-29 21:41:53 +0800

    Fixed merge issues
    - Merged componentDidMount functions
    - Added currentUser into state

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
 import { Container, Header, Content, Footer, Title,
 		Button, Text, Left, Right, Body, Icon, Card, CardItem } from 'native-base';
-import { StyleSheet, Platform, FlatList,
-		Modal, Image, View } from 'react-native';
+import { FlatList, Modal, Image, View, Dimensions, TouchableOpacity } from 'react-native';
 
 import * as firebase from 'firebase'
 
 export default class Home extends React.Component {
-	state = { currentUser: null }
-  componentDidMount() {
-    const { currentUser } = firebase.auth()
-    this.setState({ currentUser })
-  }
 	constructor(props) {
 		    super(props);
 
 		    this.state = {
+		    	currentUser: null,
 		    	loading: true,
 		    	modalVisible: false,
 		    	posts: [],
@@ -25,68 +20,73 @@ export default class Home extends React.Component {
 		    	currLocation: '',
 		    	currTime: 0
 		    };
-		}
+	}
 
-		makeRemoteRequest = () => {
-		    firebase.database().ref('posts').on('value', (snap) => {
-		        var items = [];
-		        this.getItems(snap, items);
-		        // items = items.reverse();
-	        	this.setState(
-		            {posts: items}
-		        );
-		    });
-		}
+	makeRemoteRequest = () => {
+	    firebase.database().ref('posts').on('value', (snap) => {
+	        var items = [];
+	        this.getItems(snap, items);
+	        // items = items.reverse();
+        	this.setState(
+	            {posts: items}
+	        );
+	    });
+	}
 
-		getItems = (snap, items) => {
-	        snap.forEach((child) => {
-	            items.push({
-	                key: child.key,
-	                photo: child.val().photo,
-	                name: child.val().name,
-	                location: child.val().location,
-	                time: child.val().time,
-	            });
-	        });
-	    }
+	getItems = (snap, items) => {
+        snap.forEach((child) => {
+            items.push({
+                key: child.key,
+                photo: child.val().photo,
+                name: child.val().name,
+                location: child.val().location,
+                time: child.val().time,
+            });
+        });
+    }
 
-		componentDidMount() {
-			this.makeRemoteRequest();
-		}
+	componentDidMount() {
+		const user = firebase.auth()
+		this.setState({currentUser:user})
+		this.makeRemoteRequest();
+	}
 
 	render() {
-		const { currentUser } = this.state
-
 		return (
 			<Container>
-				 <Modal
-					animationType='fade'
-					transparent={false}
-					visible={this.state.modalVisible}
-					onRequestClose={()=>this.setState({modalVisible:false})}
-					>
-						<Header>
-							<Left>
-								<Button transparent onPress={()=>this.setState({modalVisible:false})}>
-									<Icon type='Entypo' name='chevron-left' />
-								</Button>
-							</Left>
-							<Body>
-								<Title>Post Details</Title>
-							</Body>
-							<Right />
-						</Header>
-						<Content>
+				<Modal
+				animationType='fade'
+				transparent={false}
+				visible={this.state.modalVisible}
+				onRequestClose={()=>this.setState({modalVisible:false})}
+				>
+					<Header>
+						<Left>
+							<Button transparent onPress={()=>this.setState({modalVisible:false})}>
+								<Icon type='Entypo' name='chevron-left' />
+							</Button>
+						</Left>
+						<Body>
+							<Title>Post Details</Title>
+						</Body>
+						<Right />
+					</Header>
+					<Content>
+						<Image
+							source={{uri: this.state.currPhoto}}
+							style={{height: 300, width: Dimensions.get('window').width, flex:1}}
+						/>
+						<View style={{
+				        	flexDirection: 'column',
+				        	padding: 20,
+				        }}>
 							<Text>Name: {this.state.currName}</Text>
 							<Text>Key: {this.state.currKey}</Text>
 							<Text>Loc: {this.state.currLocation}</Text>
 							<Text>Time: {this.state.currTime}</Text>
-							<Text>Photo URL: {this.state.currPhoto}</Text>
-							<Button onPress={()=>this.setState({modalVisible:false})}>
-								<Text>Cancel</Text>
-							</Button>
-						</Content>
-					</Modal>
+						</View>
+					</Content>
+				</Modal>
 
 				<Header>
 					<Left>
@@ -97,34 +97,41 @@ export default class Home extends React.Component {
 					<Body>
 						<Title>Home</Title>
 					</Body>
-
 					<Right>
 						<Button transparent>
 							<Icon type='Entypo' name='plus' />
 						</Button>
 					</Right>
 				</Header>
-
+				
 				<Content>
 					<FlatList
 						data = {this.state.posts}
-
+						
 						renderItem={({item}) =>
+							<TouchableOpacity onPress={()=> this.setState({
+								modalVisible:true,
+								currKey: item.key,
+								currPhoto: item.photo,
+								currName: item.name,
+								currLocation: item.location,
+								currTime: item.time,
+							})}>
 							<Card>
-								<CardItem button onPress={()=> this.setState({
-											modalVisible:true,
-											currKey: item.key,
-											currPhoto: item.photo,
-											currName: item.name,
-											currLocation: item.location,
-											currTime: item.time,
-								})}>
-								<Body>
-									<Text>Loc: {item.location}</Text>
-									<Text>Time: {item.time}</Text>
-								</Body>
+								<CardItem cardBody>
+									<Image
+										source={{uri: item.photo}}
+										style={{height: 200, width: Dimensions.get('window').width, flex:1}}
+									/>
+								</CardItem>
+								<CardItem>
+									<Body>
+										<Text>Loc: {item.location}</Text>
+										<Text>Time: {item.time}</Text>
+									</Body>
 								</CardItem>
 							</Card>
+							</TouchableOpacity>
 						}
 					/>
 				</Content>
